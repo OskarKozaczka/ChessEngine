c8d491a19fcdc2fe8c85f396caab92b53e010b0d
OskarKozaczka <oskaro0@vp.pl>
2021-04-27 02:06:40 +0200

    Update Game.py

--- Game.py
+++ Game.py
@@ -51,7 +51,6 @@ def debug():
     print(KingInCheck)
     print(IsKingInCheck(ChessBoard,WhiteToMove))
 
-
 while True:
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONUP:
